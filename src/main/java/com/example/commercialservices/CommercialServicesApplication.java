3c5cfb4bf8f02449e7187daa82eb928080845f0c
정태승 <hi563@g.skku.edu>
2024-05-19 17:55:15 +0900

    [init]

---
+++ src/main/java/com/example/commercialservices/CommercialServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercialservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercialServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommercialServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commercialservices/CommercialServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commercialservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercialServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
